229fc2b9f1ec6d6fd5573ce02d636adbfb8d4a71
leejay <1103137221@gm.kuas.edu.tw>
2017-03-24 20:50:57 +0800

    hello world

---
+++ ASP_homework/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_homework.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Desc = "Hello world";
+            return View();
+        }
+    }
+}
