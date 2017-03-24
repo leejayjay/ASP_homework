1e6308768004e07d33344b895af4937fc0406840
leejay <1103137221@gm.kuas.edu.tw>
2017-03-24 20:45:04 +0800

    加入專案檔案。

---
+++ ASP_homework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_homework.Startup))]
+namespace ASP_homework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
