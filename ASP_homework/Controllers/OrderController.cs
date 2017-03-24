2b0ac9a02ca3ab4691a321e89cde5cc40c2d9b9d
leejay <1103137221@gm.kuas.edu.tw>
2017-03-24 22:03:05 +0800

    OrderService

---
+++ ASP_homework/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_homework.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService OrderService = new Models.OrderService();
+            var order = OrderService.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+    }
+}
---
+++ ASP_homework/Models/OrderService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_homework.Models
+{
+    /// <summary>
+    /// 訂單的服務
+    /// </summary>
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        public void InsertOrder()
+        {
+
+        }
+        /// <summary>
+        /// 刪除
+        /// </summary>
+        public void DeleteOrderById()
+        {
+
+        }
+        /// <summary>
+        /// 更新
+        /// </summary>
+        public void UpdateOrder()
+        {
+
+        }
+        /// <summary>
+        /// 取得
+        /// </summary>
+        /// <param name="id">訂單ID</param>
+        /// <returns></returns>
+        public Models.Order GetOrderById(string id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "GSS";
+            result.CustName="叡揚資訊";
+            return result;
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Order> GetOrder()
+        {
+            return new List<Order>();
+        }
+
+        
+    }
+}
